4334408dcc62310050cda18a2e3cf6a9cd7d9699
ThrashPandaG59 <daniel.martin.gonzalez@alumnojoyfe.iepgroup.es>
2024-10-23 20:11:27 +0200

    mejor

--- Pokemon/src/App.vue
+++ Pokemon/src/App.vue
@@ -6,14 +6,6 @@ import Frontpokemon from './components/frontpokemon.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
     <Frontpokemon/>
   </main>
